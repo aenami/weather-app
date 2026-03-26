99a7feb7aa4130f07b37713f38c6e80f35274e77
Julian <juliannandres05@gmail.com>
2026-03-26 14:57:08 -0500

    .

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { WeatherProvider } from './context/providerContext.tsx'
+import { WeatherProvider } from './context/ProviderContext.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
